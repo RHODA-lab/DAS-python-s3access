b6bd859bf1ebb59803fee0f3e96c4dcafb749a42
omveda <vedadashan@gmail.com>
2022-08-30 08:52:14 -0700

    fix syntax

--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,9 @@ application = Flask(__name__)
 
 
 @application.route('/')
+@application.route('/status')
+def status():
+    if 'BUCKET_HOST' in os.environ:
+    	return jsonify({'status': 'Bucket OBC is present'})
+    else:
+    	return jsonify({'status': 'Bucket OBC is absent'})
