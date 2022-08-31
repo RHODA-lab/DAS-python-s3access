0a642c9083367e76d8bbdfeca1056b8e5455c4de
omveda <vedadashan@gmail.com>
2022-08-30 22:37:07 -0700

    check for env

--- s3access-internal.py
+++ s3access-internal.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import requests
 import json
 import time
@@ -10,10 +11,24 @@ from boto3 import session
 
 key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-bucket_host = 'http://' + os.environ.get('BUCKET_HOST')
+bucket_host = os.environ.get('BUCKET_HOST')
 bucket_name = os.environ.get('BUCKET_NAME')
+if (key_id is None or secret_key is None or bucket_host is None or bucket_name is None):
+  sys.exit('Environment variable with Object Bucket Claim info has not been set')
 
-print("bucket_host:", bucket_host)
-print("bucket_name:", bucket_name)
-print("key_id:", key_id)
-print("secret_key:", secret_key)
+bucket_host = 'http://' + bucket_host
+s3 = boto3.client('s3','', endpoint_url= bucket_host,
+    aws_access_key_id = key_id,
+    aws_secret_access_key = secret_key)
+
+for x in s3.list_buckets()['Buckets']:
+  current_bucket = x['Name']
+  print("----Objects in Bucket: ", current_bucket)
+  for object in s3.list_objects(Bucket=current_bucket)['Contents']:
+    print(object['Key'], str(object))
+
+"""
+# For creating an object called "junk_object1" with some random data
+s3.put_object(Bucket=bucket_name,Key='junk_object1',Body='hello world 11111111')
+"""
+
